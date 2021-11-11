30c2bc783b658f1693f4c33dde6b5e9de261f9d0
Vitali Tatarintev <Kalastiuz@gmail.com>
2021-11-11 13:51:36 +0100

    Read the port from environment variable
    Or use 4000 as default

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -19,5 +20,10 @@ func main() {
 		w.Write([]byte(`{"items":[{"title":"Google", "link":"https://google.com"}, {"title":"Apple", "link":"https://apple.com"}]}`))
 	})
 
-	http.ListenAndServe(":4000", r)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
+
+	http.ListenAndServe(":"+port, r)
 }
