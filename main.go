2fc90dc19ce694d76ce79da97b25821579dc2476
Vitali Tatarintev <Kalastiuz@gmail.com>
2021-11-22 15:44:38 +0100

    Extract routes

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-
 	"github.com/ck3g/newnews-api/handlers"
 )
 
@@ -18,15 +15,17 @@ func main() {
 	app := &application{
 		Handlers: &handlers.Handlers{},
 	}
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/health", app.Handlers.Health)
-	r.Get("/", app.Handlers.Home)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "4000"
 	}
 
-	http.ListenAndServe(":"+port, r)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	srv.ErrorLog.Fatal(err)
 }
