17de4089751c54357f30b35697ed56d4f192805a
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-01-24 09:31:24 +0100

    Define ItemsDatastorage interface

--- data/models.go
+++ data/models.go
@@ -7,13 +7,20 @@ import (
 var db *pgx.Conn
 
 type Models struct {
-	Items ItemsModel
+	Items ItemsDatastorage
+}
+
+type ItemsDatastorage interface {
+	AllNew() ([]*Item, error)
+	Create(item Item) (int64, error)
+	Find(id int64) (*Item, error)
+	Destroy(id int64)
 }
 
 func New(databasePool *pgx.Conn) Models {
 	db = databasePool
 
 	return Models{
-		Items: ItemsModel{DB: db},
+		Items: &ItemsModel{DB: db},
 	}
 }
