348302b4c47df4dd0a9355fd9ac62247270a5c3f
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-02-08 11:16:41 +0100

    Add auth_sessions migrations
    Refactor DB setup and teardown

--- data/pgdb/testdata/setup.sql
+++ data/pgdb/testdata/setup.sql
@@ -1,25 +1,4 @@
--- TODO: Run existing migrations instead
--- Or implement some sort of `db:test:prepare`
-
-CREATE TABLE IF NOT EXISTS items(
-  id bigserial PRIMARY KEY,
-  created_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  updated_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  points int NOT NULL DEFAULT 0,
-  title VARCHAR(1024) NOT NULL,
-  link VARCHAR(2048),
-  from_site VARCHAR(128)
-);
-
-CREATE TABLE IF NOT EXISTS users(
-  id bigserial PRIMARY KEY,
-  created_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  updated_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  karma int NOT NULL DEFAULT(0),
-  username varchar(50) NOT NULL,
-  email varchar(255),
-  hashed_password CHAR(60) NOT NULL
-);
+-- Ensure test database is migrated
 
 INSERT INTO items (title, link, from_site, points) VALUES (
   'Google',
